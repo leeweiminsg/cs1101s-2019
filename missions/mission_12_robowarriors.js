5b1f001dd7acac6b01187939f81100cbbeab4c59
Lee Wei Min <40821628+leeweiminsg@users.noreply.github.com>
2019-10-05 22:29:28 +0800

    Mission 12 (Robowarriors) Submission

---
+++ missions/mission_12_robowarriors.js
@@ -0,0 +1,65 @@
+// Question 1
+const motorA = ev3_motorA();
+const motorB = ev3_motorB();
+
+ev3_runForDistance(motorA, 210, 100);
+ev3_runForDistance(motorB, 210, 100);
+
+// Question 2
+const motorA = ev3_motorA();
+const motorB = ev3_motorB();
+
+ev3_runForDistance(motorA, -220, 100);
+ev3_runForDistance(motorB, 220, 100);
+
+// Question 3
+const motorA = ev3_motorA();
+const motorB = ev3_motorB();
+const color = ev3_colorSensor();
+
+function run(a) {
+  if (a === 5) {
+    ev3_stop(motorA);
+    ev3_stop(motorB);
+  } else if (ev3_reflectedLightIntensity(color) > 30) {
+    ev3_runForDistance(motorA, 30, 100);
+    ev3_runForDistance(motorB, 30, 100);
+    run(a);
+  } else {
+    ev3_pause((1000 * 220) / 100);
+    ev3_runForDistance(motorA, -80, 100);
+    ev3_runForDistance(motorB, -80, 100);
+
+    ev3_runForDistance(motorA, -170, 100);
+    ev3_runForDistance(motorB, 170, 100);
+    ev3_pause((1000 * 220) / 100);
+    run(a + 1);
+  }
+}
+
+run(0);
+
+// Question 4
+const motorA = ev3_motorA();
+const motorB = ev3_motorB();
+const color = ev3_colorSensor();
+
+// Folllow right edge
+function run() {
+  if (ev3_reflectedLightIntensity(color) > 20) {
+    ev3_pause(500);
+    // Turn left
+    // 75
+    ev3_runForDistance(motorB, 100, 300);
+  } else {
+    // Turn right
+    ev3_pause(500);
+    ev3_runForDistance(motorA, 100, 300);
+  }
+  ev3_pause(1500);
+  ev3_runForDistance(motorB, -20, 75);
+  ev3_runForDistance(motorA, -20, 75);
+  return run();
+}
+
+run();
