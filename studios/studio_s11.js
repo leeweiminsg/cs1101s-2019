8d8ade2464687062113654e6feedca3e99f99b9f
Lee Wei Min <40821628+leeweiminsg@users.noreply.github.com>
2019-10-28 11:51:13 +0800

    Studio S11 Add Suggested Solutions

--- studios/studio_s11.js
+++ studios/studio_s11.js
@@ -1,5 +1,5 @@
 // Question 1
-// Each number in stream A is multiplied by 2
+// Each number in stream A is 2^(its index)
 
 // Each number in stream B is multiplied by (its index + 1)
 
@@ -79,6 +79,12 @@ const s2 = stream_pairs2(integers); */
 // Part E
 // pair(1, 2), pair(1, 3), pair(1, 4) ......
 
+function stream_append(xs, ys) {
+  return is_null(xs())
+    ? ys
+    : pair(head(xs()), stream_append(ys, stream_tail(xs())));
+}
+
 // Question 2
 const alt_ones = pair(1, () => pair(-1, () => alt_ones));
 
@@ -95,7 +101,6 @@ return fun_to_series(x => x + 1);
 
 // Question 4
 function mul_series(s1, s2) {
-  return pair(head(s1), () =>
-    add_series(mul_series(s1, stream_tail(s2)), mul_series(stream_tail(s1), s2))
-  );
+  return pair(head(s1) * head(s2), () =>
+    add_series(scale_stream(head(s2), stream_tail(s1)), mul_series(s1, stream_tail(s2))));
 }
