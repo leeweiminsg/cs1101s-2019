e7bb76ccaf8d38db6bc47b32fe95c3fb0aaa20f1
Lee Wei Min <40821628+leeweiminsg@users.noreply.github.com>
2019-11-13 19:56:24 +0800

    L13 Add Suggested Solutions

---
+++ practice/lecture_13_practice.js
@@ -0,0 +1,40 @@
+// Question 1
+let A = [];
+
+function stream_to_array(s, n) {
+  for (let i = 0; i < n; i = i + 1) {
+    if (is_null(s)) {
+      A[i] = 0;
+    } else {
+      A[i] = head(s);
+      s = stream_tail(s);
+    }
+  }
+
+  return A;
+}
+
+// Tests
+const s = pair(10, () => pair(20, () => null));
+stream_to_array(s, 3); // returns [10, 20, 0]
+
+// Question 2
+function array_to_stream(A) {
+  return build_stream(array_length(A), i => A[i]);
+}
+
+// Tests
+const a = [];
+a[0] = 10;
+a[2] = 30;
+array_to_stream(a);
+// returns stream containing 10, undefined , 30
+
+// Question 3
+function extend(bno) {
+  function bso(xss, yss) {
+    return pair(bno(head(xss), head(yss)), () =>
+      bso(stream_tail(xss), stream_tail(yss))
+    );
+  }
+}
